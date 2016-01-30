2b28fb9ddc23dc6f2f863ea011ed8c09539dab2e
pacificLiving <kyle.corbelli@gmail.com>
2016-01-29 16:48:20 -0800

    Pass tests for SongQueue and SongModel

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,25 @@
 var SongQueue = Songs.extend({
 
   initialize: function() {
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    });
+
+    this.on('ended', function() {
+      this.shift();
+      if (this.length > 0) {
+        this.playFirst();
+      }
+    });
+
+    this.on('dequeue', function(song) {
+      this.remove(song);
+    });
+  },
+  playFirst: function() {
+    this.at(0).play();
   }
 
 });
